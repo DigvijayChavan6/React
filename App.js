582c68e7bb909c9e11c8f9c376cd7f6a5b2a39e2
Digvijay Chavan <chavandigvijay9081@gmail.com>
2024-06-21 08:43:05 +0530

    Begin

---
+++ App.js
@@ -0,0 +1,22 @@
+const myEle = (<div id="parent">
+        <div id="child">
+                <h1 id="baccha">I am H1</h1>
+        </div>
+</div>
+);
+
+// const myEle = React.createElement(
+//         "div",
+//         { id: "parent" },
+//         React.createElement(
+//                 "div",
+//                 { id: "child" },
+//                 React.createElement("h1", {}, "Hello I am H1")
+//         )
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(myEle);
+
+// const heading=React.createElement('h1', {}, "hello, I am H1");
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
